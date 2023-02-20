80bfdc368b96c9d2f102334ef1501f07b8d56aae
Jean Silva <jeanpsilva10@gmail.com>
2023-02-20 18:45:33 -0300

    Iniciando a adição da tabela por time

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('.container')
+const local = ["Casa", "Fora"]
 const teams = [
     {
         name: "América",
@@ -99,23 +100,115 @@ const teams = [
         name: "Vasco da Gama",
         image: "vasco.svg",
         state: "Rio de Janeiro",
+        matches:[
+            {   number: 1,
+                adversary: "atletico.svg",
+                local: "Fora"
+            },
+            {
+                number: 2,
+                adversary: "palmeiras.svg",
+                local: "Casa"
+            },
+            {
+                number: 3,
+                adversary: "bahia.svg",
+                local: "Casa"
+            },
+            {
+                number: 4,
+                adversary: "fluminense.svg",
+                local: "Fora"
+            },
+            {
+                number: 5,
+                adversary: "coritiba.svg",
+                local: "Fora"
+            },
+            {
+                number: 6,
+                adversary: "santos.svg",
+                local: "Casa"
+            },
+            {
+                number: 7,
+                adversary: "sao-paulo.svg",
+                local: "Fora"
+            },
+            {
+                number: 8,
+                adversary: "fortaleza.svg",
+                local: "Fora"
+            },
+            {
+                number: 9,
+                adversary: "flamengo.svg",
+                local: "Casa"
+            },
+            {
+                number: 10,
+                adversary: "internacional.svg",
+                local: "Fora"
+            },
+            {
+                number: 11,
+                adversary: "goias.svg",
+                local: "Casa"
+            },
+            {
+                number: 12,
+                adversary: "cuiaba.svg",
+                local: "Casa"
+            },
+            {
+                number: 13,
+                adversary: "botafogo.svg",
+                local: "Fora"
+            },
+            {
+                number: 14,
+                adversary: "cruzeiro.svg",
+                local: "Casa"
+            },
+            {
+                number: 15,
+                adversary: "america.svg",
+                local: "Fora"
+            },
+            {
+                number: 16,
+                adversary: "athletico.svg",
+                local: "Casa"
+            },
+            {
+                number: 17,
+                adversary: "corinthians.svg",
+                local: "Fora"
+            },
+            {
+                number: 18,
+                adversary: "gremio.svg",
+                local: "Casa"
+            },
+            {
+                number: 19,
+                adversary: "bragantino.svg",
+                local: "Fora"
+            },
+        ]   
+    }
+]
 
-    }
-]
-const tableGames = [
-    {
-        
-    }
-]
 teams.forEach((team)=>{
+    
     container.innerHTML = container.innerHTML + `
     <div class="team ${team.image.split(".svg").join("")}">
             <img src="./assets/img/${team.image}" alt="">
             <h2>${team.name}</h2>
             <p>${team.state}</p>
-        
     </div>
-    `   
+    ` 
+   
 })
 const containerGames = document.querySelector('.container-games')
 const games = document.querySelector('.games')
@@ -123,6 +216,7 @@ const btnClose = document.querySelector('.close')
 const content = document.querySelector('.content')
 
 const team = document.querySelectorAll('.team');
+const containerContent = document.querySelector('.container-content')
 team.forEach((card)=>{
     card.addEventListener('click', ()=>{
         containerGames.style.display = 'flex'
@@ -136,14 +230,26 @@ team.forEach((card)=>{
             if(nameClass===nameImage){
             content.innerHTML = `
                 <div class="team ${team.image.split(".svg").join("")}">
-                        <h2>Jogos</h2>
+                    <p>Tabela</p>
                         <img src="./assets/img/${team.image}" alt="">
                 </div>
+                <div class="container-content">
+                </div>
             `   
+             team.matches.forEach((match)=>{
+                content.innerHTML = content.innerHTML +
+                `
+                <div class="matches">
+                <span>${match.number}º</span>
+                <img src="./assets/img/${match.adversary}" alt="">
+                <span>- ${match.local}</span>
+                </div>`
+            })
             }
         })
     })
 })
+
 games.classList.remove('enter-modal')
 btnClose.addEventListener('click', ()=>{
     containerGames.style.display = 'none';
